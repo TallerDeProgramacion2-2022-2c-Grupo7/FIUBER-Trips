982784dcfeaecca8c0b79667ca2c8237d8298f05
aleacevedo <ale.acevedo@live.com.ar>
2022-10-13 18:21:58 -0300

    feat: config okteto and google admin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,55 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'eslint-plugin-import'],
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'airbnb/base',
-        'prettier/@typescript-eslint',
-        'plugin:prettier/recommended',
-        'eslint:recommended',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'eslint-plugin-import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb/base',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+  },
+  rules: {
+    'func-names': 0,
+    '@typescript-eslint/no-namespace': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
-    parserOptions: {
-        ecmaVersion: 6,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-            modules: true,
-        },
+    'no-useless-constructor': 'off',
+    'no-empty-function': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+  env: {
+    node: true,
+    commonjs: true,
+    jest: true,
+    es2020: true,
+  },
+  settings: {
+    indent: ['error', 2],
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    rules: {
-        'func-names': 0,
-        '@typescript-eslint/no-namespace': 0,
-        'import/extensions': [
-            'error',
-            'ignorePackages',
-            {
-                js: 'never',
-                jsx: 'never',
-                ts: 'never',
-                tsx: 'never',
-            },
-        ],
-        'no-useless-constructor': 'off',
-        'no-empty-function': 'off',
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
-    env: {
-        node: true,
-        commonjs: true,
-        jest: true,
-        es2020: true,
-    },
-    settings: {
-        'indent': ['error', 2],
-        'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.ts', '.tsx'],
-        },
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            },
-        },
-    },
+  },
 };

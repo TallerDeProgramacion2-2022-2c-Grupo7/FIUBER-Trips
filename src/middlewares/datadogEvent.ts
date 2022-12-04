7d310f7f7059c56da884c77e98d765c07f0a6fff
Matías Huenul <matias.huenul.07@gmail.com>
2022-12-04 14:18:42 -0300

    Fix

--- src/middlewares/datadogEvent.ts
+++ src/middlewares/datadogEvent.ts
@@ -2,21 +2,29 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatsD } from 'hot-shots';
 
-import config from '../config';
+// import config from '../config';
 
 const datadogEvent = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (config.enviroment === 'development') {
-    return next();
-  }
+  // if (config.enviroment === 'development') {
+  //   return next();
+  // }
   res.on('finish', function() {
-    const dogstatsd = new StatsD({ host: 'dd-agent', port: 8125 });
-    dogstatsd.event(`${req.method} ${req.url} ${this.statusCode}`, '', {
-      alert_type: this.statusCode >= 500 ? 'error' : 'info',
-    });
+    try {
+      const dogstatsd = new StatsD({ host: '127.0.0.1', port: 8125 });
+      dogstatsd.event(
+        `${req.method} ${req.baseUrl}${req.url} ${this.statusCode}`,
+        '',
+        {
+          alert_type: this.statusCode >= 500 ? 'error' : 'info',
+        },
+        ['app_name:fiuber-trips']
+      );
+      // eslint-disable-next-line no-empty
+    } catch {}
   });
   return next();
 };

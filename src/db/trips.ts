8311fc0e89a3018013364818762f93728b5e4a8c
aleacevedo <ale.acevedo@live.com.ar>
2022-12-11 12:29:11 -0300

    feat: a little bit of refactor

--- src/db/trips.ts
+++ src/db/trips.ts
@@ -1,37 +1,13 @@
 import { Schema, model } from 'mongoose';
-
-export enum TripStatus {
-  SERCHING_DRIVER = 'searching_driver',
-  WAITING_DRIVER = 'waiting_driver',
-  ACCEPTED = 'accepted',
-  STARTED = 'started',
-  FINISHED = 'finished',
-}
-
-export interface ILatLng {
-  latitude: number;
-  longitude: number;
-}
-
-export interface IMapPoint {
-  coordinates: ILatLng;
-  description: {
-    name: string;
-    formattedAddress: { mainText: string; secondaryText: string };
-  };
-}
-
-export interface ITrip {
-  passengerId: string;
-  passengerDeviceId: string;
-  driverId?: string;
-  from: IMapPoint;
-  to: IMapPoint;
-  cost: number;
-  status: TripStatus;
-  paymentHash?: string;
-  createdAt: Date;
-}
+import { ENDPOINT_ERRORS } from '../constants/errors';
+import { ILatLng, IMapPoint, ITrip, TripStatus } from '../interfaces/trip';
+import { isDriver, isPassanger } from '../validations/attributes';
+import {
+  checkForAcceptStatus,
+  checkForCanceledStatus,
+  checkForFinishStatus,
+  checkForStartStatus,
+} from '../validations/next-status';
 
 export const LatLongSchema = new Schema<ILatLng>(
   {
@@ -61,9 +37,163 @@ const TripSchema = new Schema<ITrip>({
     enum: Object.values(TripStatus),
   },
   paymentHash: String,
+  canceledDriver: [String],
   createdAt: Date,
 });
 
 const Trip = model<ITrip>('Trip', TripSchema);
+
+export const createAndSaveTrip = (trip: ITrip) => new Trip(trip).save();
+
+export const getOneAvailableTrip = async (driverId: string) =>
+  Trip.findOneAndUpdate(
+    {
+      status: { $eq: TripStatus.SERCHING_DRIVER },
+      canceledDriver: { $ne: driverId },
+    },
+    { status: TripStatus.WAITING_DRIVER },
+    { new: true }
+  );
+
+export const acceptTripAndUpdate = async (tripId: string, driverId: string) => {
+  const trip = await Trip.findById(tripId);
+  if (!trip) {
+    throw new Error(ENDPOINT_ERRORS.tripNotFound);
+  }
+  const { error } = checkForAcceptStatus(trip);
+  if (error) {
+    throw new Error(error);
+  }
+
+  return Trip.findByIdAndUpdate(
+    trip.id,
+    {
+      driverId,
+      status: TripStatus.ACCEPTED,
+    },
+    { new: true }
+  );
+};
+
+export const rejectTripAndUpdate = async (tripId: string, driverId: string) => {
+  const trip = await Trip.findById(tripId);
+  if (!trip) {
+    throw new Error(ENDPOINT_ERRORS.tripNotFound);
+  }
+  const { error } = checkForAcceptStatus(trip);
+  if (error) {
+    throw new Error(error);
+  }
+
+  const updatedCanceledDriver = [...trip.canceledDriver.slice(0, 4), driverId];
+
+  return Trip.findByIdAndUpdate(
+    trip.id,
+    {
+      status: TripStatus.SERCHING_DRIVER,
+      canceledDriver: updatedCanceledDriver,
+    },
+    { new: true }
+  );
+};
+
+export const startTripAndUpdate = async (tripId: string) => {
+  const trip = await Trip.findById(tripId);
+
+  if (!trip) {
+    throw new Error(ENDPOINT_ERRORS.tripNotFound);
+  }
+  const { error } = checkForStartStatus(trip);
+  if (error) {
+    throw new Error(error);
+  }
+  return Trip.findByIdAndUpdate(
+    trip.id,
+    {
+      status: TripStatus.STARTED,
+    },
+    { new: true }
+  );
+};
+
+export const finishTripAndUpdate = async (tripId: string, driverId: string) => {
+  const trip = await Trip.findById(tripId);
+  if (!trip) {
+    throw new Error(ENDPOINT_ERRORS.tripNotFound);
+  }
+  const { error } = checkForFinishStatus(trip);
+  if (error) {
+    throw new Error(error);
+  }
+
+  const { error: attrError } = isDriver(trip, driverId);
+  if (attrError) {
+    throw new Error(attrError);
+  }
+
+  const updatedTrip = await Trip.findByIdAndUpdate(
+    trip.id,
+    {
+      status: TripStatus.FINISHED,
+    },
+    { new: true }
+  );
+
+  if (!updatedTrip || updatedTrip === null) {
+    throw new Error(ENDPOINT_ERRORS.tripNotFound);
+  }
+
+  return updatedTrip;
+};
+
+export const cancelTripAndUpdate = async (
+  tripId: string,
+  passangerId: string
+) => {
+  const trip = await Trip.findById(tripId);
+  if (!trip) {
+    throw new Error(ENDPOINT_ERRORS.tripNotFound);
+  }
+
+  const { error } = checkForCanceledStatus(trip);
+  if (error) {
+    throw new Error(error);
+  }
+
+  const { error: attrError } = isPassanger(trip, passangerId);
+  if (attrError) {
+    throw new Error(attrError);
+  }
+
+  const updatedTrip = await Trip.findByIdAndUpdate(
+    trip.id,
+    {
+      status: TripStatus.CANCELED,
+    },
+    { new: true }
+  );
+
+  if (!updatedTrip || updatedTrip === null) {
+    throw new Error(ENDPOINT_ERRORS.tripNotFound);
+  }
+
+  return updatedTrip;
+};
+
+export const getUnfinishedTrip = async (userId: string) => {
+  const trip = await Trip.findOne({
+    $and: [
+      { $or: [{ passengerId: userId }, { driverId: userId }] },
+      {
+        $and: [
+          { status: { $ne: TripStatus.FINISHED } },
+          { status: { $ne: TripStatus.CANCELED } },
+        ],
+      },
+    ],
+  });
+
+  return trip;
+};
 
 export default Trip;
